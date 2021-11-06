fef7ca77de0f96c43f117a9d185ad244a39b7657
Luiz-om <luizfernando_martins@hotmail.com>
2021-11-06 17:50:48 -0400

    Criação da classe Programa; Execução

---
+++ src/br/com/azi/aprendendojava/aplicacao/Programa.java
@@ -0,0 +1,12 @@
+package br.com.azi.aprendendojava.aplicacao;
+
+import br.com.azi.aprendendojava.dominio.Colecionador;
+import br.com.azi.aprendendojava.dominio.Item;
+
+import java.util.Arrays;
+
+public class Programa {
+    public static void main(String[] args) {
+
+    }
+}
