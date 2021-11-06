ba1bd5b16319c56aafa3ae8b6564331a41df7fd1
Luiz-om <luizfernando_martins@hotmail.com>
2021-11-06 16:55:21 -0400

    Refatoração: Retirada de construtor vazio.

--- src/br/com/azi/aprendendojava/dominio/Item.java
+++ src/br/com/azi/aprendendojava/dominio/Item.java
@@ -4,10 +4,6 @@ public class Item {
     private String nome;
     private int anoCriacao;
 
-    //Construtor vazio
-    public Item() {
-
-    }
     //Construtor com parametros
     public Item(String _nome, int _anoCriacao) {
         this.nome = _nome;
