6f23d3b35456a7f92d5219d78d01c00728e1eea7
Luiz-om <luizfernando_martins@hotmail.com>
2021-11-06 16:47:14 -0400

    Criação do construtor de Item

--- src/br/com/azi/aprendendojava/dominio/Item.java
+++ src/br/com/azi/aprendendojava/dominio/Item.java
@@ -4,5 +4,13 @@ public class Item {
     private String nome;
     private int anoCriacao;
 
+    //Construtor vazio
+    public Item() {
 
+    }
+    //Construtor com parametros
+    public Item(String _nome, int _anoCriacao) {
+        this.nome = _nome;
+        this.anoCriacao = _anoCriacao;
+    }
 }
